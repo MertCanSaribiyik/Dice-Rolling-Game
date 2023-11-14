dfab0a329eebd6aebd9dbf33ad6b0a3646e3d548
MertCanSaribiyik <mertcansaribiyik@hotmail.com>
2023-11-14 20:24:49 +0300

    Bug Fixed

--- game-init.js
+++ game-init.js
@@ -5,6 +5,9 @@ class Initialize {
     playerNames = [];
     mainMenuContainer = document.querySelector(".main-menu .container");
     numberArr = [];
+    isStartBtnClicked = false;
+    isBackBtnClicked = false;
+    isRandNameBtnClicked = false;
 
     initialize() {
         const button = document.querySelector(".main-menu .container .left button");
@@ -27,7 +30,7 @@ class Initialize {
                 //Player names detection : 
                 this.playerNamesDetection();
             }
- 
+
         });
     }
 
@@ -36,74 +39,91 @@ class Initialize {
 
         for (let i = 0; i < this.playerCount; i++)
             formItem[i].classList.remove("display-none");
-
     }
 
     playerNamesDetection() {
         const rightDiv = document.querySelector(".main-menu .container .right");
         const formItem = document.querySelectorAll(".main-menu .right .form-item");
         const buttons = rightDiv.lastElementChild;
+        const backBtn = buttons.firstElementChild;
+        const startBtn = buttons.lastElementChild
 
-        //For random name : 
-        rightDiv.firstElementChild.addEventListener("click", (e) => {
-            if (e.target.classList.contains("fa-shuffle")) {
-                const input = e.target.parentElement.parentElement.lastElementChild;
-                input.value = this.randomNameGenertor();
-            }
-        });
+        if (!this.isRandNameBtnClicked) {
+            this.isRandNameBtnClicked = true;
 
-        //When clicking the back button : 
-        buttons.firstElementChild.addEventListener("click", (e) => {
-            e.preventDefault();
-
-            //Transition animation : 
-            this.transition();
-
-            //Form and buttons deleted : 
-            setTimeout(() => {
-                for (let i = 0; i < this.playerCount; i++) {
-                    formItem[i].classList.add("display-none");
-                    formItem[i].lastElementChild.value = "";
+            //For random name : 
+            rightDiv.firstElementChild.addEventListener("click", (e) => {
+                if (e.target.classList.contains("fa-shuffle")) {
+                    const input = e.target.parentElement.parentElement.lastElementChild;
+                    input.value = this.randomNameGenertor();
                 }
-            }, 500);
+            });
+        }
 
-            this.numberArr = [];
-        }, { once: true });
 
-        //When clicking the start game  button : 
-        buttons.lastElementChild.addEventListener("click", (e) => {
-            e.preventDefault();
+        if (!this.isBackBtnClicked) {
+            this.isBackBtnClicked = true;
 
-            const input = document.querySelectorAll(".main-menu .container .right form .form-item input");
-            let errorInput = 0;
+            //When clicking the back button : 
+            backBtn.addEventListener("click", (e) => {
+                e.preventDefault();
 
-            //Initial value assigment to player names array :
-            for (let i = 0; i < this.playerCount; i++) {
-                if (!this.textControl(input[i].value)) {
-                    errorInput++;
+                //Transition animation : 
+                this.transition();
+
+                //Form and buttons deleted : 
+                setTimeout(() => {
+                    for (let i = 0; i < this.playerCount; i++) {
+                        formItem[i].classList.add("display-none");
+                        formItem[i].lastElementChild.value = "";
+                    }
+                }, 500);
+
+                this.numberArr = [];
+            });
+
+        }
+
+        if (!this.isStartBtnClicked) {
+            this.isStartBtnClicked = true;
+
+            //When clicking the start game  button : 
+            startBtn.addEventListener("click", (e) => {
+                e.preventDefault();
+
+                const input = document.querySelectorAll(".main-menu .container .right form .form-item input");
+                let errorInput = 0;
+
+                //Initial value assigment to player names array :
+                for (let i = 0; i < this.playerCount; i++) {
+                    if (!this.textControl(input[i].value)) {
+                        errorInput++;
+                        continue;
+                    }
+
+                    this.playerNames.push(input[i].value);
+                }
+
+                if (errorInput > 0) {
+                    this.playerNames = [];
                 }
 
                 else {
-                    this.playerNames.push(input[i].value);
+                    console.log(this.playerNames.length);
+                    //Create game : 
+                    let game = new Game(this.playerNames);
+                    game.createGameArea();
                 }
-            }
 
-            if (errorInput > 0) {
-                this.playerNames = [];
-                return;
-            }
+                this.numberArr = [];
+            });
+        }
 
-            this.numberArr = [];
-             
-            //Create game : 
-            let game = new Game(this.playerNames);
-            game.createGameArea();      
 
-        }, { once: true });
     }
 
     numberControl(num) {
-        if (num >= "2" && num <= "6")
+        if (num >= 2 && num <= 6)
             return true
 
         return false;
